7c5225242f44b11c55037c2be74fc3a2a1fe6e8c
SajadAzami <sajjadaazami@gmail.com>
2016-05-15 21:12:23 +0430

    MusicShop-DB Relations were added

--- Database Homeworks/MusicShop-DB/music_shop_tables.sql
+++ Database Homeworks/MusicShop-DB/music_shop_tables.sql
@@ -2,16 +2,6 @@ CREATE DATABASE MusicShop;
 
 USE MusicShop;
 
-
-############### Includes information about tracks ###############
-CREATE TABLE `Track` (
-  `track_id`   INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `album_id`   INT             NOT NULL,
-  `track_name` VARCHAR(32)     NOT NULL
-);
-ALTER TABLE `Track`
-  COMMENT = 'Includes information about tracks';
-
 ############### Includes information about albums ###############
 CREATE TABLE `Album`
 (
@@ -23,6 +13,30 @@ CREATE TABLE `Album`
 ALTER TABLE `Album`
   COMMENT = 'Includes information about albums';
 
+############### Includes information about users ###############
+CREATE TABLE `User`
+(
+  `user_id`   INT PRIMARY KEY NOT NULL,
+  `user_name` VARCHAR(32)     NOT NULL,
+  `password`  VARCHAR(32)     NOT NULL
+);
+ALTER TABLE `User`
+  COMMENT = 'Includes information about users';
+
+############### Includes information about tracks ###############
+CREATE TABLE `Track` (
+  `track_id`   INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `album_id`   INT             NOT NULL,
+  `track_name` VARCHAR(32)     NOT NULL,
+  FOREIGN KEY (album_id)
+  REFERENCES Album (album_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+
+);
+ALTER TABLE `Track`
+  COMMENT = 'Includes information about tracks';
+
 ############### Includes information about artists ###############
 CREATE TABLE `Artist`
 (
@@ -33,28 +47,37 @@ CREATE TABLE `Artist`
 ALTER TABLE `Artist`
   COMMENT = 'Includes information about artists';
 
-############### Includes information about comments ###############
-CREATE TABLE `Comment`
-(
-  `comment_id` INT         NOT NULL AUTO_INCREMENT,
-  `user_id`    VARCHAR(32) NOT NULL,
-  `rate`       INT(1)      NOT NULL,
-  `comment`    VARCHAR(32) NOT NULL,
-  `track_id`   INT         NOT NULL,
-  PRIMARY KEY (comment_id, track_id)
-);
-ALTER TABLE `Comment`
-  COMMENT = 'Includes information about comments';
-
 ############### Includes information about composes ###############
 CREATE TABLE `Composes`
 (
   `track_id`  INT NOT NULL,
   `artist_id` INT NOT NULL,
-  PRIMARY KEY (track_id, artist_id)
+  PRIMARY KEY (track_id, artist_id),
+  FOREIGN KEY (track_id)
+  REFERENCES Track (track_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (artist_id)
+  REFERENCES Artist (artist_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 ALTER TABLE `Composes`
   COMMENT = 'Includes information about composer';
+
+############### Includes information about order ###############
+CREATE TABLE `Orders`
+(
+  `user_id`  INT NOT NULL,
+  `order_id` INT NOT NULL,
+  PRIMARY KEY (user_id, order_id),
+  FOREIGN KEY (user_id)
+  REFERENCES User (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+ALTER TABLE `Orders`
+  COMMENT = 'Includes information about orders';
 
 ############### Includes information about order ###############
 CREATE TABLE `Order`
@@ -62,20 +85,18 @@ CREATE TABLE `Order`
   `track_id` INT NOT NULL,
   `user_id`  INT NOT NULL,
   `order_id` INT NOT NULL,
-  PRIMARY KEY (track_id, user_id, order_id)
+  PRIMARY KEY (track_id, user_id, order_id),
+  FOREIGN KEY (user_id, order_id)
+  REFERENCES Orders (user_id, order_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (track_id)
+  REFERENCES Track (track_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 ALTER TABLE `Order`
   COMMENT = 'Includes information about order';
-
-############### Includes information about order ###############
-CREATE TABLE `Orders`
-(
-  `user_id`  INT NOT NULL,
-  `order_id` INT NOT NULL,
-  PRIMARY KEY (user_id, order_id)
-);
-ALTER TABLE `Orders`
-  COMMENT = 'Includes information about orders';
 
 ############### Includes information about genres ###############
 CREATE TABLE `Genre`
@@ -89,9 +110,17 @@ ALTER TABLE `Genre`
 ############### Includes information about which track is in which genre ###############
 CREATE TABLE `TrackGenres`
 (
-  `track_id` INT         NOT NULL,
-  `genre_id` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (track_id, genre_id)
+  `track_id` INT NOT NULL,
+  `genre_id` INT NOT NULL,
+  PRIMARY KEY (track_id, genre_id),
+  FOREIGN KEY (track_id)
+  REFERENCES Track (track_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (genre_id)
+  REFERENCES Genre (genre_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 ALTER TABLE `TrackGenres`
   COMMENT = 'Includes information about which track is in which genre';
@@ -99,9 +128,17 @@ ALTER TABLE `TrackGenres`
 ############### Includes information about which album is in which genre ###############
 CREATE TABLE `AlbumGenres`
 (
-  `album_id` INT         NOT NULL,
-  `genre_id` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (album_id, genre_id)
+  `album_id` INT NOT NULL,
+  `genre_id` INT NOT NULL,
+  PRIMARY KEY (album_id, genre_id),
+  FOREIGN KEY (album_id)
+  REFERENCES Album (album_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (genre_id)
+  REFERENCES Genre (genre_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 ALTER TABLE `AlbumGenres`
   COMMENT = 'Includes information about which album is in which genre';
@@ -109,20 +146,40 @@ ALTER TABLE `AlbumGenres`
 ############### Includes information about which artist is in which genre ###############
 CREATE TABLE `ArtistGenres`
 (
-  `artist_id` INT         NOT NULL,
-  `genre_id`  VARCHAR(32) NOT NULL,
-  PRIMARY KEY (artist_id, genre_id)
+  `artist_id` INT NOT NULL,
+  `genre_id`  INT NOT NULL,
+  PRIMARY KEY (artist_id, genre_id),
+  FOREIGN KEY (artist_id)
+  REFERENCES Artist (artist_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (genre_id)
+  REFERENCES Genre (genre_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
 ALTER TABLE `ArtistGenres`
   COMMENT = 'Includes information about which artist is in which genre';
 
-############### Includes information about users ###############
-CREATE TABLE `User`
+############### Includes information about comments ###############
+CREATE TABLE `Comment`
 (
-  `user_id`   INT PRIMARY KEY NOT NULL,
-  `user_name` VARCHAR(32)     NOT NULL,
-  `password`  VARCHAR(32)     NOT NULL
+  `comment_id` INT         NOT NULL AUTO_INCREMENT,
+  `user_id`    INT         NOT NULL,
+  `rate`       INT(1)      NOT NULL,
+  `comment`    VARCHAR(32) NOT NULL,
+  `track_id`   INT         NOT NULL,
+  PRIMARY KEY (comment_id, track_id),
+  FOREIGN KEY (user_id)
+  REFERENCES User (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (track_id)
+  REFERENCES Track (track_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+
 );
-ALTER TABLE `User`
-  COMMENT = 'Includes information about users';
+ALTER TABLE `Comment`
+  COMMENT = 'Includes information about comments';
 
