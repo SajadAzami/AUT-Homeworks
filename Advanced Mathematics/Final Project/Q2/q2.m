e183ffc74765ccb8bcf553b81490df204a7954b4
Sajad Azami <sajadazami@aut.ac.ir>
2017-01-17 02:56:52 +0330

    all done, needs some fixes for plotting part

--- Advanced Mathematics/Final Project/Q2/q2.m
+++ Advanced Mathematics/Final Project/Q2/q2.m
@@ -1,5 +1,8 @@
 %% Question 2: Approximation of an image using Sigular Value Decomposition
+clear;
 input_image = imread('image_test.jpg');
 input_image = rgb2gray(input_image);
 input_image_double = double(input_image);
-image(svd_approximator(input_image_double));
+fileinfo = dir('image_test.jpg');
+original_size = fileinfo(1).bytes;
+image(svd_approximator(input_image_double, original_size));
--- Advanced Mathematics/Final Project/Q2/svd_approximator.m
+++ Advanced Mathematics/Final Project/Q2/svd_approximator.m
@@ -1,38 +1,43 @@
-function [ approximated_image ] = svd_approximator( input_image )
+function [ approximated_image ] = svd_approximator( input_image, original_size )
 %SVD_APPROXIMATOR Approximates an Image by given rank using SVD
-rank = 50;
 [U,S,V]=svd( input_image );
-dispEr = [];
-numSVals = [];
+ranks_to_be_tested = [ 5 10 20 50 100 ];
+compression_ratios = [];
+errors = [];
 
-for rank = [ 5, 20 ]
+for rank = ranks_to_be_tested
+    % Fill other diagonal values with zeros
     C = S;
     C(rank + 1:end, :) = 0;
     C(:, rank + 1:end) = 0;
+    
     % Reconstruct Image
     approximated_image = U*C*V';
     
-    figure;
-    title_text = sprintf('Rank %d Approximation', rank);
-    imshow(uint8( approximated_image ));
-    title( title_text );
-    error=sum(sum((input_image-approximated_image).^2));
-
-    % store vals for display
-    dispEr = [dispEr; error];
-    numSVals = [numSVals; rank];
+    % Write the compressed image to file
+    file_name = strcat(num2str(rank), '_approximation.jpg');
+    imwrite(uint8( approximated_image ), file_name, 'jpg');
+    
+    % Calculate compressed image size and compression ratio
+    fileinfo = dir(file_name);
+    new_file_size = fileinfo(1).bytes;
+    ratio = new_file_size / original_size;
+    compression_ratios(end+1) = ratio;    
+    
+    % Plot the compressed image
+    figure(rank);
+    title_text = sprintf('Rank %d Approximation | Size: %d Bytes | Compression Ratio: %f', rank, new_file_size, ratio);
+    imshow(uint8(approximated_image));
+    title(title_text);
+    
+    % Compute MSE
+    errors(end+1) = sum(sum((input_image - approximated_image).^2));
 
 end
-disp(dispEr);
-disp(numSVals);
-% dislay the error graph
-figure;
-plot(numSVals, dispEr);
-%figure(5); 
-%title('Error in compression');
-%plot(numSVals, dispEr);
-%grid on
-%xlabel('Number of Singular Values used');
-%ylabel('Error between compress and original image');
-
+disp('-----------');
+disp(ranks_to_be_tested);
+disp('-----------');
+disp(compression_ratios);
+disp('-----------');
+disp(errors);
 end
