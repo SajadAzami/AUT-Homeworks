44e0647c51fcf3810a0aff5387e3f1e46a788f12
Sajad Azami <sajadazami@aut.ac.ir>
2017-01-18 04:14:51 +0330

    face recognition completed

--- Advanced Mathematics/Final Project/Q3(Bonus)/image_read.m
+++ Advanced Mathematics/Final Project/Q3(Bonus)/image_read.m
@@ -1,6 +1,7 @@
 function [ image ] = image_read( address )
 %IMAGE_READ reads image from address
 image = imread(address);
+image = imresize(image, 0.5); % Reduce to 0.25 if needed
 image = rgb2gray(image);
 image = im2double(image);
 end
--- Advanced Mathematics/Final Project/Q3(Bonus)/q3.m
+++ Advanced Mathematics/Final Project/Q3(Bonus)/q3.m
@@ -1,53 +1,56 @@
 %% Question 3 (Bonus): Face Recognition With Singular Value Decomposition
-clear;
-% Load Dataset
+% Load one of images to work with sizes
 input_image = image_read(strcat('data_set/train/', '1.jpg'));
-
-% Size of each image
-M  = size(input_image(), 1) * size(input_image, 2);
 
 % Number of individuals
 N = 19; 
+% Size of each image
+M  = size(input_image(), 1) * size(input_image, 2);
+
 
 % Creating the train data matrix
 S = zeros(M, N); 
 for i = 1:N
-temp = image_read(sprintf('data_set/train/%d.jpg', i));
-S(:, i) = temp(:);
+    temp = image_read(sprintf('data_set/train/%d.jpg', i));
+    S(:, i) = temp(:);
 end
 
 % Computing the mean image of train data
 train_mean = mean(S, 2);
 
 % Normalizing images by subtracting mean
-S = S - train_mean(:, ones(1, N));
+A = S - train_mean(:, ones(1, N));
 
 % Performing the Singular Value Decomposition over A
-[u, s, v] = svd(S);
-rank = size(s, 2);
+[u, s, v] = svd(A);
 
-% Computing coorinate vector xi for each known individual
-xi = u(:, 1:N)' * A;
+% Computing coordinate vector xi for each known individual
+rank = size(A, 2); 
+xi = u(:, 1:rank)' * A;
 
-% Choosing thresholds
-epsilon_0 = 180; % Maximum allowable distance from any known face in the training set S
-epsilon_1 = 9; % Maximum allowable distance from face space
+% Defining thresholds, these values were defined by trial and error
+epsilon_0 = 50; % Maximum allowable distance from any known face in the training set S
+epsilon_1 = 15; % Maximum allowable distance from face space
 
 % Classification
-epsilon_i = zeros(N, 1);
-test_image = image_read('data_set/2/9338535.19.jpg');
+images = ['1' '2' '7' '13' '16' 'not_a_face' 'unknown1' 'unknown2' 'unknown3' ]; % Test Images Available
+epsilons = zeros(N, 1);
+test_image = image_read('data_set/test/not_a_face.jpg');
 test_image = test_image(:) - train_mean; % Normalizing test image
-x = u(:, 1:N)' * test_image; % Calculating coordinate vector x
-tmp = test_image - u(:, 1:N) * x;
-epsilon_f = (tmp' * tmp) ^ .5;
+x = u(:, 1:rank)' * test_image; % Calculating coordinate vector x of test image
+epsilon_f = ((test_image - u(:, 1:rank) * x)' * (test_image - u(:, 1:rank) * x)) ^ 0.5;
 
+% Checks if it is in face space
 if epsilon_f < epsilon_1
-    for j = 1:N
-        tmp = xi(:, j) - x;
-        epsilons(j, 1) = tmp' * tmp;
+    % Computing distance epsilon_i to the face space
+    for i = 1:N
+        epsilons(i, 1) = (xi(:, i) - x)' * (xi(:, i) - x);
     end
-    if min(epsilons(:, 1)) < epsilon_0
-        disp('');
+    [val idx] = min(epsilons(:, 1));
+    if val < epsilon_0
+        disp(sprintf('The face belongs to %d', idx));
+    else
+        disp('Unknown face');
     end
 else
     disp('Input image is not a face');
